82c70b19636cc88c94744ec7d43823e0421ed76d
Gwazza23 <weerawarnagayan@gmail.com>
2023-06-20 10:01:30 +0100

    Add framer-motion dependency

--- src/util/RouteList.js
+++ src/util/RouteList.js
@@ -1,19 +1,27 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import { AnimatePresence } from "framer-motion";
+
 import HomePage from "../pages/HomePage/HomePage";
 import RegisterPage from "../pages/AuthenticationPage/RegisterPage";
 import LoginPage from "../pages/AuthenticationPage/LoginPage";
 import NavBar from "../pages/NavBar/NavBar";
 
 function RouteList() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<NavBar />}>
-        <Route index element={<HomePage />} />
-      </Route>
-    </Routes>
+    <AnimatePresence initial={false} mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
   );
 }
 
