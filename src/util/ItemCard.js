0e48ee751c2bbdc52538f5ace153c832cd123910
Gwazza23 <weerawarnagayan@gmail.com>
2023-06-21 15:43:36 +0100

    update mobile responsivness for new-releases component

--- src/util/ItemCard.js
+++ src/util/ItemCard.js
@@ -1,13 +1,34 @@
 import "./ItemCard.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 function ItemCard({ item }) {
-  const [mouseOn, SetMouseOn] = useState(false);
+  const [mouseOn, setMouseOn] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isAnimationEnabled, setIsAnimationEnabled] = useState(true);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    setIsAnimationEnabled(windowWidth >= 430);
+  }, [windowWidth]);
 
   const handleMouseEnter = () => {
-    SetMouseOn(!mouseOn);
+    setMouseOn(!mouseOn);
   };
+
   const divVariant = {
     initial: {
       scale: 1,
@@ -34,13 +55,15 @@ function ItemCard({ item }) {
       opacity: 1,
     },
   };
+
   return (
     <motion.div
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseEnter}
       variants={divVariant}
-      whileHover={"hover"}
-      initial={"initial"}
+      whileHover={isAnimationEnabled ? "hover" : ""}
+      whileFocus={isAnimationEnabled ? "hover" : ""}
+      initial={isAnimationEnabled ? "initial" : ""}
       className="item-card-div"
     >
       <img src={item.img_url} alt={item.name} />
@@ -48,7 +71,16 @@ function ItemCard({ item }) {
       <motion.p variants={pVariant} className="item-price">
         {item.price}
       </motion.p>
-      <motion.p className="buy-now" variants={buyNowVariant} initial='initial' animate={mouseOn? 'visible' : 'hidden'}>In-Stock:{item.quantity}</motion.p>
+      {windowWidth >= 430 && (
+        <motion.p
+          className="buy-now"
+          variants={buyNowVariant}
+          initial={isAnimationEnabled ? "initial" : ""}
+          animate={isAnimationEnabled ? (mouseOn ? "visible" : "hidden") : ""}
+        >
+          In-Stock: {item.quantity}
+        </motion.p>
+      )}
     </motion.div>
   );
 }
